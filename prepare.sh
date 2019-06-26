e0c41a35e7a1f78323237170c4b018659b9b4e8e
rundel <alexrndl@gmail.com>
2019-06-26 20:43:16 +0300

    build script

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 git clone https://github.com/alexkernphysiker/RectangularScintillator.git
 git clone https://github.com/alexkernphysiker/math_h.git
+cd math_h
+#ToDo checkout
+cd ..
 git clone https://github.com/google/googletest.git
-
+mkdir build
+cd build
+cmake ..
+if make; then
+math_h/math_h_test.exe
+RectangularScintillator/rectscin-test.exe
+fi
+cd ..
