072c40aee21abaa961beff32b80d023866678bdd
rundel <alexrndl@gmail.com>
2019-07-02 23:22:01 +0300

    closer to previous calculations

--- simulation-0.cpp
+++ simulation-0.cpp
@@ -48,7 +48,7 @@ const vector<vector<pair<double,double>>> si_phm_matrix={
 };
 int main(int , char **){
     list<double> lengths={30, 50, 75, 100, 150, 200, 300, 400, 500, 1000, 1400, 2000};
-    SortedPoints<> curve1,curve2,curve3;
+    SortedPoints<> curve1,curve21,curve22,curve3;
     for(auto L:lengths){
         {//photomultipliers
           cout<<L<<"mm"<<endl;
@@ -66,19 +66,24 @@ int main(int , char **){
           }
           cout<<"Creating system 2"<<endl;
           auto scin2=withabsorption(L);
-          auto time_difference2=make_shared<SignalStatictics>();
+          auto time_difference21=make_shared<SignalStatictics>();
+          auto time_difference22=make_shared<SignalStatictics>();
           {
-            auto left=make_shared<SignalSortAndSelect>(2),right=make_shared<SignalSortAndSelect>(2);
+            auto left1=make_shared<SignalSortAndSelect>(0),right1=make_shared<SignalSortAndSelect>(0);
+            auto left2=make_shared<SignalSortAndSelect>(2),right2=make_shared<SignalSortAndSelect>(2);
             for(const auto& size_m:si_phm_matrix){
                 auto photosensor=[&size_m](){return Photosensor(size_m,1.0,Si_Photo_QE,tts);};
                 auto l=make_shared<Signal>(),r=make_shared<Signal>();
                 scin2->Surface(0,RectDimensions::Left)>>(photosensor()>>(TimeSignal({make_pair(0,1)})>>l));
                 scin2->Surface(0,RectDimensions::Right)>>(photosensor()>>(TimeSignal({make_pair(0,1)})>>r));
-                left<<l;right<<r;
+                left1<<l;right1<<r;
+                left2<<l;right2<<r;
             }
-            auto inv_right=SignalInvert();
-            right>>inv_right;
-            (make_shared<SignalSumm>()<<left<<inv_right)>>time_difference2;
+            auto inv_right1=SignalInvert();
+            auto inv_right2=SignalInvert();
+            right1>>inv_right1;right2>>inv_right2;
+            (make_shared<SignalSumm>()<<left1<<inv_right1)>>time_difference21;
+            (make_shared<SignalSumm>()<<left2<<inv_right2)>>time_difference22;
           }
           cout<<"Creating system 3-a"<<endl;
           auto scin3=withabsorption(L);
@@ -109,7 +114,8 @@ int main(int , char **){
           }
           cout<<"Getting points 1,2"<<endl;
           curve1<<make_point(L,(time_difference1->data()+DOI).uncertainty());
-          curve2<<make_point(L,(time_difference2->data()+DOI).uncertainty());
+          curve21<<make_point(L,(time_difference21->data()+DOI).uncertainty());
+          curve22<<make_point(L,(time_difference22->data()+DOI).uncertainty());
           cout<<"Creating system 3-b"<<endl;
           auto scin3_final=withabsorption(L);
           auto time_difference3=make_shared<SignalStatictics>();
@@ -151,8 +157,9 @@ int main(int , char **){
     }
     Plot("0")
     .Line(curve1,"tube + absorption + DOI","0-1")
-    .Line(curve2,"2x5 matrix (3rd) + absorption + DOI","0-2")
-    .Line(curve3,"2x5matrix(wieghted) + absorption","0-3")
+    .Line(curve21,"2x5 matrix (1st) + absorption + DOI","0-2")
+    .Line(curve22,"2x5 matrix (3rd) + absorption + DOI","0-3")
+    .Line(curve3,"2x5matrix(wieghted) + absorption","0-4")
     <<"set key on";
     return 0;
 }
