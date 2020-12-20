bd14d8b6fcaef1b0a05169b74eee7ce6a9fd423b
Oleksandr Rundel <alexrndl@gmail.com>
2020-12-20 20:21:37 +0200

    silicon photosensors matrix

--- model.h
+++ model.h
@@ -6,5 +6,5 @@ extern const MathTemplates::LinearInterpolation<> polyester_absorp;
 extern const MathTemplates::LinearInterpolation<> Si_Photo_QE;
 extern const MathTemplates::LinearInterpolation<> tube_QE;
 const int N_photons=2700;
-const int virtual_experiments_count=10000;
+const int virtual_experiments_count=5000;
 #endif
--- simulation-matrix.cpp
+++ simulation-matrix.cpp
@@ -24,7 +24,7 @@ const auto DOI = value<>(0.0,0.063);//ns
 
 //Scintillator
 const double opt_dens=1.58;
-const auto sizeX=make_pair(0.0,6.0);//milimeters
+const auto sizeX=make_pair(0.0,12.0);//milimeters
 const auto sizeY=make_pair(0.0,30.0);//milimeters
 const auto sizeZ=make_pair(-150.0,150.0);//milimeters
 double absorption(const double&lambda){
@@ -40,7 +40,6 @@ const double z_ph = 0.0;
 
 
 int main(){
-
   //Scintillator
   auto scin1=MakeScintillator(
             {sizeZ,sizeX,sizeY},opt_dens,TimeDistribution2(0.005,0.2,1.5),
@@ -48,40 +47,38 @@ int main(){
   );
   scin1->Configure(Scintillator::Options(1,50));//1 thread, max 50 reflections
 
-  //The block gathering statistics of the time difference between two signals
   vector<shared_ptr<SignalStatictics>> time_differences;
-      {
-        //Creating photosensor covering all butt of the scintillator
-        // the parameters: covered surface, the efficiency of the optical glue, quantum efficiency, time resolution
-        auto photosensor=[](){return Photosensor({sizeX,sizeY},1.0,Si_Photo_QE.func(),tts);};
-        //glue photosensors to the scintillator butts
-        auto left_sensor=photosensor(), right_sensor=photosensor();
-        scin1->Surface(0,RectDimensions::Left) >> left_sensor;
-        scin1->Surface(0,RectDimensions::Right) >> right_sensor;
+  {
+        auto left_sorter = make_shared<SignalSort>(), right_sorter = make_shared<SignalSort>();
+        for(size_t i=0; i<5; i++)for(size_t j=0; j<2; j++){
+            auto photosensor=[i,j](){return Photosensor(
+                            {make_pair( 6.0*j, 6.0*(j+1) ),make_pair( 6.0*i, 6.0*(i+1) )},
+                            1.0,Si_Photo_QE.func(),tts);};
+            auto left_sensor=photosensor(), right_sensor=photosensor();
 
-        for(size_t index=0; index<80; index++){//cycle over different order statistics
-            // virtual wires to conduct the "signal" corresponding to needed poton's registration time
+            scin1->Surface(0,RectDimensions::Left) >> left_sensor;
+            scin1->Surface(0,RectDimensions::Right) >> right_sensor;
+
             auto left_wire=make_shared<Signal>(), right_wire=make_shared<Signal>();
 
-            // TimeSignal({make_pair(index,1)}) : the time of signal is obtained for the order statistics of index
-            left_sensor >> ( TimeSignal({make_pair(index,1)}) >> left_wire );
-            right_sensor >> ( TimeSignal({make_pair(index,1)}) >> right_wire );
+            left_sensor >> ( TimeSignal({make_pair(0,1)}) >> left_wire );
+            right_sensor >> ( TimeSignal({make_pair(0,1)}) >> right_wire );
 
-            //Invert time of signal from the right photomultiplier
-            auto inv_right=SignalInvert();
-            right_wire >> inv_right;
+            left_sorter << left_wire;
+            right_sorter << right_wire;
+        }
+        for(size_t index=0; index<(2*5); index++){
+            auto left_wire=make_shared<Signal>(), right_wire=make_shared<Signal>();
 
-            //Add left signal with the inverted right signal and connect the sum to the block gathering statistics
+            left_sorter >> left_wire;
+            right_sorter >> (SignalInvert() >> right_wire);
+
             auto time_difference = make_shared<SignalStatictics>();
-            (make_shared<SignalSumm>()<<left_wire<<inv_right)>>time_difference;
-
-            //pushing this statistics to vector
+            ( make_shared<SignalSumm>()<<left_wire<<right_wire ) >> time_difference;
             time_differences.push_back(time_difference);
         }
+   }
 
-      }
-
-   //repeat simulating registering gammas
    for(unsigned int cnt=0;cnt<virtual_experiments_count;cnt++){
         scin1->RegisterGamma( {z_ph,x_ph(),y_ph()}, N_photons);
    }
@@ -92,7 +89,7 @@ int main(){
             res_by_order_statistics << make_point( (double)(i+1), (time_differences[i]->data() + DOI).uncertainty() );
    }
 
-   Plot("order_resolution").Line(res_by_order_statistics)<<"set key on"
+   Plot("matrix_resolution").Line(res_by_order_statistics)<<"set key on"
                                                        << "set xlabel 'photosensor order statistics'"
                                                        << "set ylabel 'time resolution'" ;
   return 0;
